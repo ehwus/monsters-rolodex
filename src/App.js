4020c73904dcd8f36e071cbafbe27350851c41ec
Alex Withington-Smith <alex@awithsmith.com>
2021-08-21 07:39:42 +0100

    added styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component";
-import { SearchBox } from './components/search-box/search-box.component'
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -12,6 +12,10 @@ class App extends Component {
         };
     }
 
+    handleChange = (e) => {
+        this.setState({searchField: e.target.value});
+    };
+
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then((res) => res.json())
@@ -21,14 +25,13 @@ class App extends Component {
     render() {
         const {monsters, searchField} = this.state;
         const filteredMonsters = monsters.filter(monster =>
-            monster.name.toLowerCase().includes(searchField.toLowerCase())
+            monster.name.toLowerCase().includes(searchField.toLowerCase()),
         );
 
         return (
             <div className="App">
-                <SearchBox placeholder="Search monsters" handleChange={(e) => {
-                    this.setState({searchField: e.target.value});
-                }} />
+                <h1>Monsters Rolodex</h1>
+                <SearchBox placeholder="Search monsters" handleChange={this.handleChange}/>
                 <CardList monsters={filteredMonsters}/>
             </div>
         );
